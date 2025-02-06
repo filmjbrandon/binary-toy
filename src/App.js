2fdd987c3c876b2b09dac1e2a3fd8ae83810f34f
Jim Kass <jimk@alternativereality.com>
2025-02-06 09:19:40 -0800

    removing old/unused files

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import './App.css';
-import BitInteraction from './components/BitInteraction';
-function App() {
-    return (_jsx("div", { className: "App", children: _jsx(BitInteraction, {}) }));
-}
-export default App;
